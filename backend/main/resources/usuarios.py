1473d75d512728355555fc5d32f15e3b98782217
Franco Vaccarezza <vaco@MacBook-Pro-Vaco.local>
2025-03-28 11:11:15 -0300

    usuario

---
+++ backend/main/resources/usuarios.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource
+from flask import request
+
+USUARIOS = {
+    1: {'nombre': 'Admin', 'rol': 'ADMIN'},
+    2: {'nombre': 'User1', 'rol': 'USER'}
+}
+
+# Definir el recurso Usuarios
+class Usuarios(Resource):
+    def get(self):
+        return USUARIOS
+    
+    def post(self):
+        usuario = request.get_json()
+        id = int(max(USUARIOS.keys())) + 1
+        USUARIOS[id] = usuario
+        return USUARIOS[id], 201
