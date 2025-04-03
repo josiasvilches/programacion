8adb3ffec784fdc6b40a66ea9cee10a6c576b221
josiasvilches <jl.vilches@alumno.um.edu.ar>
2025-04-02 22:03:41 -0300

    correciones y se agregó postman

--- backend/main/resources/pedido_id.py
+++ backend/main/resources/pedido_id.py
@@ -8,21 +8,21 @@ PEDIDOS = {
 
 class Pedido(Resource):
     def get(self, id):
-        if id in PEDIDOS:
-            return PEDIDOS[id], 200
+        if int(id) in PEDIDOS:
+            return PEDIDOS[int(id)]
         else:
             return "Pedido no encontrado", 404
         
     def delete(self, id):
-        if id in PEDIDOS:
-            del PEDIDOS[id]
+        if int(id) in PEDIDOS:
+            del PEDIDOS[int(id)]
             return "Eliminado con éxito", 204
         else:
             return "Pedido no encontrado", 404
         
     def put(self, id):
-        if id in PEDIDOS:
-            pedido = PEDIDOS[id]
+        if int(id) in PEDIDOS:
+            pedido = PEDIDOS[int(id)]
             data = request.get_json()
             pedido.update(data)
             return "Modificado correctamente.", 201
